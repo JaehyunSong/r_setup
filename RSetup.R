3e7eedadf8d9af06c29b8185840bb4e9f68b0ae5
Jaehyun Song <tintstyle@gmail.com>
2021-09-06 11:45:19 +0900

    no message

---
+++ RSetup.R
@@ -0,0 +1,69 @@
+install.packages("devtools")
+install.packages("remotes")
+install.packages("pacman")
+
+# Data Handling
+install.pacakges("xlsx")
+install.pacakges("haven")
+install.packages("tidyverse")
+install.packages("fastDummies")
+
+# For R Markdown
+install.packages("knitr")
+install.packages("rmarkdown")
+install.packages("kableExtra")
+install.packages("bookdown")
+install.packages("blogdown")
+install.packages("xaringan")
+install.packages("chromote")
+install.packages("DT")
+remotes::install_github("gadenbuie/xaringanExtra")
+remotes::install_github("jhelvy/xaringanBuilder")
+
+# For Shiny
+install.packages("shiny")
+install.packages("shiny.i18n")
+install.packages("bslib")
+install.packages("shinyBS")
+install.packages("shinyjs")
+
+# For Visualization
+install.packages("GGally")
+install.pacakges("gghighlight")
+install.pacakges("ggExtra")
+install.pacakges("ggridges")
+install.pacakges("rnaturalearth")
+install.pacakges("rnaturalearthdata")
+install.packages("ggalluvial")
+install.packages("treemapify")
+install.packages("ggbump")
+install.packages("dendextend")
+install.packages("ggdendro")
+install.packages("ragg")
+install.packages("ghibli")
+install.packages("hrbrthemes")
+install.packages("ggdag")
+install.packages("daggity")
+remotes::install_github("uribo/jpndistrict")
+remotes::install_github("haleyjeppson/ggmosaic")
+remotes::install_github("mitchelloharawild/icons")
+
+# For Estimation
+install.packages("estimatr")
+install.packages("rstan")
+install.packages("margins")
+install.packages("prediction")
+install.packages("rdd")       # Regression Discontinuity Design
+install.packages("rdrobust")  # Regression Discontinuity Design
+install.packages("rddensity") # Density test for RDD
+install.packages("cjoint") # Conjoint Analysis
+install.packages("cregg")  # Conjoint Analysis
+remotes::install_github("JaehyunSong/BalanceR") # Balance Check
+
+
+# Datasets
+install.packages("titanic")
+install.packages("gapminder")
+remotes::install_github("xmarquez/democracyData") # Polity V, Freedom House, ...
+devtools::install_github("ropengov/rqog") # Quality of Governance
+devtools::install_github("vdeminstitute/vdemdata") # V-dem
